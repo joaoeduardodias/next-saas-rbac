d0584e66ec2e108a1c9303c20dde8aae161e2dc2
João Dias <joaoeduardodias123@gmail.com>
2025-07-09 14:42:17 -0300

    feat: create route authenticate with password

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -7,9 +7,11 @@ export async function createAccount(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post('/users',
     {
       schema: {
+        tags: ['Auth'],
+        summary: "Create a new user account",
         body: z.object({
           name: z.string().min(1, "Name is required"),
-          email: z.string(),
+          email: z.email(),
           password: z.string().min(6, "Password must be at least 6 characters long"),
         })
       }
@@ -23,6 +25,16 @@ export async function createAccount(app: FastifyInstance) {
       if (userWithSameEmail) {
         return reply.status(400).send({ error: "User with same e-mail already exists." });
       }
+
+      const [, domain] = email.split('@');
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true
+        }
+      })
+
       const passwordHash = await hash(password, 6);
 
       await prisma.user.create({
@@ -30,6 +42,11 @@ export async function createAccount(app: FastifyInstance) {
           name,
           email,
           passwordHash,
+          member_on: autoJoinOrganization ? {
+            create: {
+              organizationId: autoJoinOrganization.id,
+            }
+          } : undefined,
         }
       })
       return reply.status(201).send()
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,19 +1,50 @@
 
 import fastifyCors from '@fastify/cors';
+import fastifyJwt from '@fastify/jwt';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUi from '@fastify/swagger-ui';
+
 import { fastify } from 'fastify';
 import {
-  //  jsonSchemaTransform,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider
 } from 'fastify-type-provider-zod';
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password';
 import { createAccount } from './routes/auth/create-account';
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+
+
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Fullstack Next.js SaaS application with RBAC',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+});
+
+
 app.register(fastifyCors)
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+
+})
 
 app.register(createAccount)
+app.register(authenticateWithPassword)
 
 
 
