b7d3c09df214b8ee42245c634de7fd1427d2b320
João Dias <joaoeduardodias123@gmail.com>
2025-07-09 17:14:36 -0300

    feat: create middleware

--- apps/api/src/http/routes/auth/get-profile.ts
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -1,3 +1,4 @@
+import { auth } from "@/http/middlewares/auth";
 import { prisma } from "@/lib/prisma";
 import type { FastifyInstance } from "fastify";
 import type { ZodTypeProvider } from "fastify-type-provider-zod";
@@ -5,7 +6,7 @@ import { z } from "zod/v4";
 import { BadRequestError } from "../_errors/bad-request-error";
 
 export async function getProfile(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get('/profile', {
+  app.withTypeProvider<ZodTypeProvider>().register(auth).get('/profile', {
     schema: {
       tags: ['Auth'],
       summary: 'Get authenticated user profile.',
@@ -21,7 +22,7 @@ export async function getProfile(app: FastifyInstance) {
       }
     }
   }, async (request, reply) => {
-    const { sub } = await request.jwtVerify<{ sub: string }>()
+    const userId = await request.getCurrentUserId()
     const user = await prisma.user.findUnique({
       select: {
         id: true,
@@ -30,7 +31,7 @@ export async function getProfile(app: FastifyInstance) {
         avatarUrl: true,
       },
       where: {
-        id: sub
+        id: userId
       }
     })
 
